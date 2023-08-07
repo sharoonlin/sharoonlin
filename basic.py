108b9e6e1481ddb5ed0c8c92a624592d90448505
sharoonlin <139829066+sharoonlin@users.noreply.github.com>
2023-08-07 14:15:36 +0800

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,48 @@
+from line_bot_api import *
+
+def about_us_event(event):
+    emoji =[
+        {
+            "index":0,
+            "productId":"5ac21c4e031a6752fb806d5b",
+            "emojiId":"005"
+
+        },
+        {
+            "index":17,
+            "productId":"5ac21c4e031a6752fb806d5b",
+            "emojiId":"002"
+        }
+    ]
+    text_message=TextSendMessage(text='''$ Master Finance $
+Hello 您好歡迎您成為hihi的朋友
+我是財經好幫手
+-這裡有股票匯率資訊
+-直接點選下方press it 選單功能                                                                     
+-Weclome~''',emojis=emoji)
+    
+    sticker_message = StickerSendMessage(
+        package_id='11539',
+        sticker_id='52114130'
+    )
+    line_bot_api.reply_message(
+        event.reply_token, 
+        [text_message, sticker_message])
+    
+def push_msg(event,msg):
+    try:
+        user_id = event.source.user_id
+        line_bot_api.push_message(user_id,TextSendMessage(text=msg))
+    except:
+        room_id = event.source.room_id
+        line_bot_api.push_message(room_id,TextSendMessage(text=msg))
+def Usage(event):
+        push_msg(event,"🛡️🛡️查詢方法🛡️🛡️\
+                 \n \
+                 \n 🎈小幫手可以查詢油價👉匯率👉股價\
+                 \n \
+                 \n 🎁油價通知 👉👉👉 輸入查詢油價\
+                 \n 🎁匯率通知 👉👉👉 輸入查詢匯率\
+                 \n 🎁匯率兌換 👉👉👉 換匯USD/TWD\
+                 \n 🎁股價查詢 👉👉👉 輸入#股票代號\
+                 ")
