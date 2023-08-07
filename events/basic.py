e1b49c10add1931543603ebcd7ead29e99ea9ae7
sharoonlin <139829066+sharoonlin@users.noreply.github.com>
2023-08-07 15:31:35 +0800

    Add files via upload

--- events/basic.py
+++ events/basic.py
@@ -25,9 +25,35 @@ Hello 您好歡迎您成為hihi的朋友
         package_id='11539',
         sticker_id='52114130'
     )
+
+    buttons_template =TemplateSendMessage(
+        alt_text='小幫手 template',
+        template=ButtonsTemplate(
+            title='選擇服務',
+            text='請選擇',
+            thumbnail_image_url='https://i.imgur.com/K84QPW3.jpeg',
+            actions=[
+    
+                MessageTemplateAction(
+                    label='油價查詢',
+                    text='油價查詢'
+                ),
+                MessageTemplateAction(
+                     label='匯率查詢',
+                     text='匯率查詢'
+                ),
+                MessageTemplateAction(
+                    label='股價查詢',
+                    text='股價查詢'
+                )
+            ]
+
+        )
+    )
     line_bot_api.reply_message(
         event.reply_token, 
         [text_message, sticker_message])
+    
     
 def push_msg(event,msg):
     try:
