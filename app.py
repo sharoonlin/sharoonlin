fca24977818408e8ddf2f4f9ac21a0e4fd709b9e
sharoonlin <139829066+sharoonlin@users.noreply.github.com>
2023-08-07 16:11:40 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from line_bot_api import *
 from events.basic import *
+from oil import *
 app=Flask(__name__)
-
 #處理訊息
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -25,11 +25,17 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message_text = str(event.message.text).lower()
+    #####################使用說明 選單 油價查詢
 
     if message_text =='@使用說明':
         about_us_event(event)
         Usage(event)
-       
+
+    if event.message.text == "想知道油價":
+        content = oil_price()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))   
 
 if __name__ == "__main__":
     app.run()
