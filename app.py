1d218c27aee71811536f142c5ce9ced0e8cd10b5
sharoonlin <139829066+sharoonlin@users.noreply.github.com>
2023-08-07 11:58:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#載入linebot 所需的軟件
+#載入linebot 所需的套件
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler, exceptions)
 from linebot.exceptions import (InvalidSignatureError)
@@ -10,6 +10,7 @@ line_bot_api = LineBotApi('BWD6SYM1M59YQ8n21+p2s28YE3p9O1IQwSC7FReV4BXPvmkJracng
 #paste your "Channel Secret" 
 handler= WebhookHandler('8e632b1c1583f4817a80f3312d027e1b')
 
+#處理訊息
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -28,12 +29,36 @@ def callback():
 
     return 'OK'
 
-# 處理訊息
+# 處理訊息callback the same message 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    emoji =[
+        {
+            "index":0,
+            "productId":"5ac21c4e031a6752fb806d5b",
+            "emojiId":"005"
 
+        },
+        {
+            "index":17,
+            "productId":"5ac21c4e031a6752fb806d5b",
+            "emojiId":"002"
+        }
+    ]
+    text_message=TextSendMessage(text='''$ Master Finance $
+Hello 您好歡迎您成為hihi的朋友
+我是財經好幫手
+-這裡有股票匯率資訊
+-直接點選下方press it 選單功能                                                                     
+-Weclome~''',emojis=emoji)
+    
+    sticker_message = StickerSendMessage(
+        package_id='11539',
+        sticker_id='52114130'
+    )
+    line_bot_api.reply_message(
+        event.reply_token, 
+        [text_message, sticker_message])
 
 if __name__ == "__main__":
     app.run()
