f3c46e89f7120e9edacbbecd4377329beb9209ba
sharoonlin <139829066+sharoonlin@users.noreply.github.com>
2023-08-11 14:47:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ import re #有爬蟲
 import twstock #股價
 import datetime
 app=Flask(__name__)
+#抓使用者設定它關心的股票
+
+def cache_users_stock():
+    db= constructor_stock()
+    nameList =db.list_collection_names()
+    users=[]
+    for i in range(len(nameList)):
+        collect =db[nameList[i]]
+        cel = list(collect.find({"tag":'stock'}))
+        users.append(cel)
+    return users
+
 #處理訊息
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -69,6 +81,18 @@ def handle_message(event):
         content=show_stock_setting(user_name,uid)
         line_bot_api.push_message(uid,TextSendMessage(content))
         return 0   
+#刪除存在資料庫裏面的股票
+    if re.match('刪除[0-9]{4}',msg):
+        content = delete_my_stock(user_name,msg[2:])
+        line_bot_api.push_message(uid,TextSendMessage(content))
+        return 0
+#清空存在資料庫裡面的股票
+    if re.match('清空股票',msg):
+        content = delete_my_all_stock(user_name,msg[2:])
+        line_bot_api.push_message(uid,TextSendMessage(content))
+        return 0 
+
+#清空存在資料庫裡面的股票
     
 
     if (msg.startswith('#')):
@@ -115,6 +139,46 @@ def handle_message(event):
         line_bot_api.push_message(uid,TextSendMessage("將為您做外匯計算...."))
         content = getExchangeRate(msg)
         line_bot_api.push_message(uid, TextSendMessage(content))
+
+    if re.match("股價提醒",msg):
+        import schedule
+        import time
+        #查看當前股價
+        def look_stock_price(stock,condition,price,userID):
+            print(userID)
+            url='https://tw.stock.yahoo.com/q/q?s=' + stock 
+            list_req =request.get(url)
+            soup=BeautifulSoup(list_req.content,"html.parser")
+            getstock =soup.findAll('b')[1].text
+            content = stock+"當前股市價格為" + getstock
+            if condition == '<':
+                content+= "\n篩選條件為:<"+price
+                if float(getstock) <float(price):
+                    content +="\n符合" + getstock +" < " + price +"的篩選條件" 
+                    line_bot_api.push_message(userID,TextSendMessage(text=content))
+            elif condition =='>':
+                content+="\n篩選條件為:> "+ price
+                if float (getstock) > float(price):
+                    content +="\n符合" + getstock +" > " + price +"的篩選條件" 
+                    line_bot_api.push_message(userID,TextSendMessage(text=content))
+            elif condition =='=':
+                content+="\n篩選條件為:= "+ price
+                if float (getstock) == float(price):
+                    content +="\n符合" + getstock +" = " + price +"的篩選條件" 
+                    line_bot_api.push_message(userID,TextSendMessage(text=content))
+    def job():
+        print('HH')
+        dataList=cache_users_stock()
+        #print(dataList)
+        for i in range(len(dataList)):
+            for k in range(len(dataList[i])):
+                #print(dataList[i][k])
+                look_stock_price(dataList[i][k]['favorite_stock'], dataList[i][k]['condition'], dataList[i][k]['price'], dataList[i][k]['userID'])
+    schedule.every(30).seconds.do(job).tag('daily-tasks-stock'+uid,'second')#每十秒執行一次
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 @handler.add(FollowEvent)
 def handle_follow(event):
